b8fb3676dafe40a3f9b37dd057b8ba68484aa301
UgnilJoZ <janolezab@aol.com>
2015-08-12 13:55:57 +0200

    Add nodes netherrack and glowstone

---
+++ init.lua
@@ -0,0 +1,1 @@
+dofile(minetest.get_modpath("nethest").."/nodes.lua")
---
+++ nodes.lua
@@ -0,0 +1,24 @@
+minetest.register_node("nethest:rack", {
+	description = "Nether Rack",
+	tiles = {"nethest_rack.png"},
+	is_ground_content = true,
+	drop = {
+		max_items = 1,
+		items = {{
+			rarity = 3,
+			items = {"nether:rack"},
+		}}
+	},
+	groups = {cracky=3,level=2},
+	sounds = default.node_sound_stone_defaults(),
+})
+
+minetest.register_node("nethest:glowstone", {
+	description = "Glowstone",
+	tiles = {"nethest_glowstone.png"},
+	is_ground_content = true,
+	light_source = 13,
+	groups = {cracky=3,oddly_breakable_by_hand=3},
+	sounds = default.node_sound_glass_defaults(),
+})
+
